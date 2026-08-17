9df547edf1806923db4ba07368b5e7db4b65683f
dominikjaro <dominikjaro789@gmail.com>
2026-08-17 12:01:16 +0100

    upload-reports.py

---
+++ upload-reports.py
@@ -0,0 +1,27 @@
+import requests
+
+headers = {
+    'Authorization': 'Token feebebd053b74f9564ff99731d10c99edb634f9b'
+}
+
+url = 'https://demo.defectdojo.org/api/v2/import-scan/'
+
+data = {
+    'active': True
+    'verified': True
+    'engagement': 37
+    'scan_type': 'GitLeaks Scan'
+    'minimum_severity': 'Low'
+    'environment': 'Test'
+}
+
+files = {
+    'file': open('gitleaks.json', 'rb')
+}
+
+response = requests.post(url, headers=headers, data=data, files=file)
+
+if response.status_code == 201
+    print('Scan results imported successfully')
+else:
+    print(f'Failed to import scan results: {response.content}')
