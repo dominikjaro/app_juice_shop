10fc35bd7d3e40c49d8ce87d7a339dcf1ee4ab6c
dominikjaro <dominikjaro789@gmail.com>
2026-08-18 13:43:11 +0100

    Add more files

--- upload-reports.py
+++ upload-reports.py
@@ -1,4 +1,15 @@
 import requests
+import sys
+
+file_name = sys.argv[1]
+scan_type = ""
+
+if file_name == "gitleaks.json":
+    scan_type = "GitLeaks Scan"
+elif file_name == "njsscan.sarif":
+    scan_type = "SARIF"
+elif file_name == "semgrep.json":
+    scan_type = "Semgrep JSON Report"
 
 headers = {
     'Authorization': 'Token feebebd053b74f9564ff99731d10c99edb634f9b'
@@ -10,13 +21,13 @@ data = {
     'active': True,
     'verified': True,
     'engagement': 37,
-    'scan_type': 'GitLeaks Scan',
+    'scan_type': scan_type,
     'minimum_severity': 'Low',
     'environment': 'Test'
 }
 
 files = {
-    'file': open('gitleaks.json', 'rb')
+    'file': open(file_name, 'rb')
 }
 
 response = requests.post(url, headers=headers, data=data, files=files)
