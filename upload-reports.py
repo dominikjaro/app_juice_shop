9c1d6e344fe53fb2c1add2e5fb908d174a4d9bce
dominikjaro <dominikjaro789@gmail.com>
2026-08-21 16:24:34 +0100

    retire

--- upload-reports.py
+++ upload-reports.py
@@ -10,6 +10,8 @@ elif file_name == "njsscan.sarif":
     scan_type = "SARIF"
 elif file_name == "semgrep.json":
     scan_type = "Semgrep JSON Report"
+elif file_name == "retire.json"
+    scan_type = "Retire.js Scan"
 
 headers = {
     'Authorization': 'Token ba3410cfd3936dfab9c9b6123d5184794bb2e87c'
