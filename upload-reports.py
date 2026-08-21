06dadf75d4101a1e9aa9f56ff1a55211c803e53c
dominikjaro <dominikjaro789@gmail.com>
2026-08-21 16:36:27 +0100

    missing :

--- upload-reports.py
+++ upload-reports.py
@@ -10,7 +10,7 @@ elif file_name == "njsscan.sarif":
     scan_type = "SARIF"
 elif file_name == "semgrep.json":
     scan_type = "Semgrep JSON Report"
-elif file_name == "retire.json"
+elif file_name == "retire.json":
     scan_type = "Retire.js Scan"
 
 headers = {
